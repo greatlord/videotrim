60ba6e2557239d5ef037d87fcae3a61160875f14
Magnus Olsen <olsen.sweden@gmail.com>
2022-12-22 23:17:35 +0100

    add source code

---
+++ Class1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using FFmpeg.NET;
+using FFmpeg.NET.Enums;
+using FFmpeg.NET.Events;
+using FFmpeg.NET.Exceptions;
+using FFmpeg.NET.Extensions;
+using FFmpeg.NET.Models;
+using FFmpeg.NET.Services;
+using System;
+
+
+
+namespace TrimVideo
+{
+    internal class ClassFfmpegProc
+    {
+
+        public static string ffmpegPath = "ffmpeg.exe";
+        private static List<string> strSilencedetect = new List<string>();
+
+        static public bool useCuda = false;
+        static public async Task runFFmpeg(string inVideoFile, string outVideoFile, string parms )
+        {
+            Engine ffmpeg;
+            
+            ffmpeg = new Engine(ffmpegPath);
+
+            ffmpeg.Progress += OnProgress;
+            ffmpeg.Data += OnData;
+            ffmpeg.Complete += OnComplete;
+            ffmpeg.Error += OnError;
+
+            if ( useCuda ) {
+                parms = "-hwaccel cuda " + "-i " + inVideoFile + " " + parms + " " + outVideoFile;
+            } else {
+                parms = "-hwaccel auto -i " + inVideoFile + " " + parms + " " + outVideoFile;
+            }
+            
+            await ffmpeg.ExecuteAsync(parms, default).ConfigureAwait(false);
+        }
+
+        private static void OnProgress(object sender, ConversionProgressEventArgs e)
+        {
+           
+            //Console.WriteLine("[{0} => {1}]", e.Input.Name, e.Output?.Name);
+            Console.WriteLine("Bitrate: {0}", e.Bitrate);
+            Console.WriteLine("Fps: {0}", e.Fps);
+            Console.WriteLine("Frame: {0}", e.Frame);
+            Console.WriteLine("ProcessedDuration: {0}", e.ProcessedDuration);
+            Console.WriteLine("Size: {0} kb", e.SizeKb);
+            Console.WriteLine("TotalDuration: {0}\n", e.TotalDuration);
+            
+            
+        }
+
+        private static void OnData(object sender, ConversionDataEventArgs e)
+        {
+            // Console.WriteLine("[{0} => {1}]: {2}", e.Input.Name, e.Output?.Name, e.Data);
+        }
+
+
+        private static void OnComplete(object sender, ConversionCompleteEventArgs e)
+        {
+            //Console.WriteLine("Completed conversion from {0} to {1}", e.Input.Name, e.Output?.Name);
+        }
+
+
+        private static void OnError(object sender, ConversionErrorEventArgs e)
+        {
+            //Console.WriteLine("[{0} => {1}]: Error: {2}\n{3}", e.Input.Name, e.Output?.Name, e.Exception.ExitCode, e.Exception.InnerException);
+        }
+
+
+
+
+        static public async Task<string> GetSilenceTrimBeignAndEnd( string inVideoFile, DB db )
+        {
+
+            Engine ffmpeg;
+            string args;
+            StringBuilder s;
+
+
+            // always clear and init global strSilencedetect 
+            strSilencedetect = new List<string>();
+
+            // use string builder it is faster that + 
+            s = new StringBuilder();
+
+            s.Append("-hide_banner ");
+
+            // input file
+            s.Append( "-i " + inVideoFile  );
+
+            // no video
+            s.Append(" -vn ");
+
+            // detect silence at begin and end of video using dB for analog audio and digtial defualt is rms works only for digtal audio
+            switch ( db )
+            {
+                case DB.db30:
+                    s.Append("-af \"silencedetect=n=-30dB\" ");
+                    break;
+
+                case DB.db35:
+                    s.Append("-af \"silencedetect=n=-35dB\" ");
+                    break;
+
+                case DB.db40:
+                    s.Append("-af \"silencedetect=n=-35dB\" ");
+                    break;
+
+                default:
+                    s.Append("-af \"silencedetect=n=-50dB\" ");
+                    break;
+            }
+
+            // We do not need a outfile so we send it to null:
+            s.Append(" -f null - ");
+
+            // setup xffmpeg library
+            ffmpeg = new Engine(ffmpegPath);
+            ffmpeg.Data += OnDataTrimInfo;
+
+            string parms = s.ToString();
+
+            // execute ffmpeg with our own custom parms
+            await ffmpeg.ExecuteAsync(parms, default).ConfigureAwait(false);
+
+            // Get the trim parms for begin and of the video. if no silence found at begin or end no parms are set then
+            args = "";
+            if ( strSilencedetect.Count >= 6 )
+            {
+                
+                string strBeignEnd;
+                string strEndStart;
+                
+
+                // the string strSilencedetect contain 3 post for each silence it find
+                // pos 0 : silence_start
+                // pos 1 : silence_end
+                // pos 2 : durations of the silence
+
+                // get the frist silence it found and start the cut where it ends
+                strBeignEnd = strSilencedetect[1].Replace("end:", "").Trim();
+
+                // get last silence start and end the cut at that point
+                strEndStart = strSilencedetect[strSilencedetect.Count - 3].Replace("start:", "").Trim();
+
+                args += "-ss " + strBeignEnd;
+                args += " -to " + strEndStart;
+
+            }
+
+            return args;
+
+        }
+
+        private static void OnDataTrimInfo(object sender, ConversionDataEventArgs e)
+        {
+            // Console.WriteLine("[{0} => {1}]: {2}", e.Input.Name, e.Output?.Name, e.Data);
+            // "[silencedetect @ 00000159f14d7ec0] silence_start: 0.0231066"
+            if (!string.IsNullOrEmpty(e.Data))
+            {
+                if (e.Data.StartsWith("[silencedetect"))
+                {
+                    if (e.Data.Contains("silence_start"))
+                    {
+                        string s = e.Data.Split("silence_")[1];
+                        strSilencedetect.Add(s);
+                    }
+
+                    if (e.Data.Contains("silence_end"))
+                    {
+                        string s = e.Data.Split("silence_")[1].Replace("|", "").Trim();
+                        strSilencedetect.Add(s);
+                        s = e.Data.Split("silence_")[2];
+                        strSilencedetect.Add(s);
+                    }
+
+
+
+                }
+            }
+
+        }
+
+
+
+
+          
+
+
+
+
+
+
+        public enum DB
+        {
+            db50 = 0, // Cuts are almost not recognizable.
+            db40 = 1, // Cuts are almost not recognizable.
+            db35 = 2, //Cut inhaling breath before speaking; cuts are quite recognizable.
+            db30 = 3, // Cut Mouse clicks and mouse movent; cuts are very recognizable."
+
+        }
+
+        private bool isUnix { 
+            get
+            {
+                var os = Environment.OSVersion;
+                if (os.Platform == PlatformID.Unix)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        private bool isWindows {
+
+            get
+            {
+                var os = Environment.OSVersion;
+                if (os.Platform == PlatformID.Win32NT)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.IO;
+
+namespace TrimVideo // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string inFile = "";
+            string outFile = "";
+            string strParms;
+            string strFFmpegParms = "";
+
+            if ( args.Length < 4 )
+            {
+                help();
+                System.Environment.Exit(-3);
+            }
+
+            if (File.Exists("ffmpeg.txt"))
+            {
+                using (StreamReader reader = new StreamReader("ffmpeg.txt"))
+                {
+                    strFFmpegParms = reader.ReadLine() ?? "";
+                    strFFmpegParms = strFFmpegParms.Trim();
+                }
+            }
+
+            TrimFileVildate(args,strFFmpegParms);
+            TrimFolderVildate(args, strFFmpegParms);
+
+            help();
+        }
+
+        static void TrimFolderVildate(string[] parms, string strFFmpegParms)
+        {
+            
+            string inFolder;
+            string outFolder;
+            string[] files;
+
+            inFolder = "";
+            outFolder = "";
+
+            for (int i = 0; i < parms.Length; i += 2)
+            {
+
+                string parm = parms[i];
+
+                if (parm == "-id") {
+                    inFolder = parms[i + 1];
+                } else if (parm == "-od") {
+                    outFolder = parms[i + 1];
+                } else if (parm == "-cuda") {
+                    if (parms[i + 1] == "on") {
+                        ClassFfmpegProc.useCuda = true;
+                    }
+                }
+
+            }
+
+            if ((string.IsNullOrEmpty(inFolder)) || (string.IsNullOrEmpty(outFolder))) {
+                return ;
+            }
+
+            if ( (!Directory.Exists(inFolder)) || (!Directory.Exists(outFolder)) )
+            {
+                System.Console.WriteLine("Folders does not exists, duble check in and out folders exists");
+                System.Environment.Exit(-7);
+            }
+
+            files = Directory.GetFiles(outFolder);
+            if (files.Length != 0)
+            {
+                Console.WriteLine("error: files in outfilder : " + outFolder);
+                System.Environment.Exit(-8);
+            }
+
+            files = Directory.GetFiles(inFolder);
+            if (files.Length == 0 )
+            {
+                Console.WriteLine("error: No files in infolder : " + inFolder);
+                System.Environment.Exit(-8);
+            }
+
+
+            const string quote = "\"";
+
+            foreach ( string inFile in files)
+            {
+                string outFile;
+                FileInfo f = new FileInfo(inFile);
+
+                outFile = outFolder + Path.DirectorySeparatorChar + f.Name;
+
+                doTrimAndVideoEnc(quote + inFile + quote, quote + outFile + quote, strFFmpegParms);
+            }
+
+
+            System.Environment.Exit(2);
+
+        }
+
+        static void TrimFileVildate( string[] parms, string strFFmpegParms)
+        {
+            string inFile;
+            string outFile;
+
+            inFile = "";
+            outFile = "";
+
+            for (int i = 0; i < parms.Length; i += 2)
+            {
+                string parm = parms[i];
+
+                if (parm == "-i") {
+                    inFile = parms[i + 1];
+                } else if (parm == "-o") {
+                    outFile = parms[i + 1];
+                } else if (parm == "-cuda") {
+                    if ( parms[i + 1] == "on" ) {
+                        ClassFfmpegProc.useCuda = true;
+                    } 
+                }
+
+            }
+
+            if ( ( string.IsNullOrEmpty(inFile)) || (string.IsNullOrEmpty(outFile)) )
+            {
+                return;
+            }
+
+            if (File.Exists(outFile))
+            {
+                Console.WriteLine("please delete output file before encoding");
+                // System.Environment.Exit(-4);
+                File.Delete(outFile);
+            }
+
+            if (!File.Exists(inFile))
+            {
+                Console.WriteLine("No file was given for input");
+                System.Environment.Exit(-5);
+            }
+
+            doTrimAndVideoEnc(inFile, outFile, strFFmpegParms);
+            System.Environment.Exit(1);
+        }
+
+        static void doTrimAndVideoEnc( string inFile, string outFile, string strCustomParms)
+        {
+            string strParms;
+         
+            // Get ffmpeg trim parms for the video
+            var procTrimVideo = ClassFfmpegProc.GetSilenceTrimBeignAndEnd(inFile, ClassFfmpegProc.DB.db50);
+            procTrimVideo.Wait();
+            strParms = procTrimVideo.Result;
+            if ( string.IsNullOrEmpty(strParms))
+            {
+                System.Environment.Exit(-6);
+            }
+
+            strParms += " " + strCustomParms;
+            strParms = strParms.Trim();
+
+            // start doing encding;
+            var procEncdoing = ClassFfmpegProc.runFFmpeg(inFile, outFile, strParms);
+            procEncdoing.Wait();
+
+        }
+
+        static void help()
+        {
+            Console.WriteLine("Simple program that auto trim video begin and end for silence audio");
+            Console.WriteLine("ffmpeg must be in same folder other wise it will fail");
+            Console.WriteLine("");
+            Console.WriteLine("Create a file call ffmpeg.txt in same folder as ffmpeg it should only contain the args/parms you want to use");
+            Console.WriteLine("-i infile : the video file you want trim audio at begin and end of the video");
+            Console.WriteLine("-o outfile : the out video after it got cut");
+            Console.WriteLine("-cuda on : turn on cuda for video filter it adding -hwaccel cuda before -i when it is on other wise it add  -hwaccel auto");
+
+            Console.WriteLine("");
+            Console.WriteLine("-id infolder : a folder with video files see -i for more info");
+            Console.WriteLine("-od outfolder : a folder to put output files in see -o for more info");
+
+
+        }
+    }
+}
